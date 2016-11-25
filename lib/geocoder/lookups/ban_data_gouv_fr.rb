ea98fe9465dd40df814d8ba565470990752cee38
Julian Nacci <julian.nacci@gmail.com>
2016-11-25 19:34:23 +0100

    add ? to query url

--- lib/geocoder/lookups/ban_data_gouv_fr.rb
+++ lib/geocoder/lookups/ban_data_gouv_fr.rb
@@ -14,8 +14,8 @@ module Geocoder::Lookup
 
     def query_url(query)
       method = query.reverse_geocode? ? "reverse" : "search"
-      "#{protocol}://api-adresse.data.gouv.fr/#{method}/" + url_query_string(query)
-      p "#{protocol}://api-adresse.data.gouv.fr/#{method}/" + url_query_string(query)
+      "#{protocol}://api-adresse.data.gouv.fr/#{method}/?" + url_query_string(query)
+      p "#{protocol}://api-adresse.data.gouv.fr/#{method}/?" + url_query_string(query)
     end
 
     private # ---------------------------------------------------------------
